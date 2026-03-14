a842cadd0d1c5fcb05be8721069a6b131f559388
nobody-lua <99367320+nobody-lua@users.noreply.github.com>
2026-03-14 17:04:32 +0100

    Bump version to 5.0.0, fix workflow .NET version to 10.0.x

--- Plugin.cs
+++ Plugin.cs
@@ -36,7 +36,7 @@ namespace Olympus;
 
 public sealed class Plugin : IDalamudPlugin
 {
-    public const string PluginVersion = "4.10.6";
+    public const string PluginVersion = "5.0.0";
     private const string CommandName = "/olympus";
 
     private readonly IDalamudPluginInterface pluginInterface;
